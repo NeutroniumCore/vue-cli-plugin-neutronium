e1599a069e6f59aee9dce9e3585208ba2f73cc01
desmaisons_david <desmaisons_david@hotmail.com>
2018-12-03 00:30:34 -0200

    Integrated script correction

--- generator/index.js
+++ generator/index.js
@@ -9,7 +9,7 @@ module.exports = (api, option) => {
   api.extendPackage({
     scripts: {
       serve: "vue-cli-service serve ./src/main.js --open --port 9000",
-      live: "vue-cli-service serve ./example/main.js --port 8080 --mode production",
+      live: "vue-cli-service serve ./src/integrated.js --port 8080 --mode production",
       build: `vue-cli-service build --target lib --name build --entry ./src/entry.js --no-clean`,
     },
     dependencies: {
