958419e1ac5ed460c1a3e0b8f71e885b2cdf05f8
desmaisons_david <desmaisons_david@hotmail.com>
2018-12-05 21:34:51 -0200

    Updating install

--- generator/template/src/install.js
+++ generator/template/src/install.js
@@ -1,17 +1,25 @@
 <% if (options.useRouter) { %>import Vue_Router from "vue-router";
-import { router } from "./route";
-<% } %>
+import { router } from "./route";<% } %>
+<% if (options.useInternationalization) { %>import VueI18n from "vue-i18n";
+import messages from "./message";<% } %>
+
 /*eslint no-unused-vars: ["error", { "args": "none" }]*/
 function install(Vue) {
   //Call vue use here if needed
 <% if (options.useRouter) { %>    Vue.use(Vue_Router);<% } %>
+<% if (options.useInternationalization) { %>    Vue.use(VueI18n);<% } %>
 }
 
 /*eslint no-unused-vars: ["error", { "args": "none" }]*/
 function vueInstanceOption(vm) {
+  <% if (options.useInternationalization) { %>const i18n = new VueI18n({
+    locale: "en-US", // set locale
+    messages // set locale messages
+  });<% } %>
   //Return vue global option here, such as vue-router, vue-i18n, mix-ins, .... 
   return {
 <% if (options.useRouter) { %>    router,<% } %>
+<% if (options.useInternationalization) { %>    i18n,<% } %>
   }
 }
 
