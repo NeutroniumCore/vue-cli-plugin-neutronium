04c7cb7e932ab9c62fe4ed8049aa89988e3e6739
desmaisons_david <desmaisons_david@hotmail.com>
2018-12-07 00:12:24 -0200

    Improve internalization

--- prompts.js
+++ prompts.js
@@ -1,22 +1,38 @@
 module.exports = [
-    {
-        type: 'confirm',
-        name: 'useRouter',
-        message: 'Use router with vue-router integrated with Neutronium?',
-        default: false,
-    },
-    {
-        type: 'confirm',
-        name: 'useInternationalization',
-        message: 'Use internationalization with vue-i18n integrated with Neutronium?',
-        default: false,
-        group: "Internationalization"
-    },
-    {
-        type: 'input',
-        name: 'projectName',
-        when: answer => answer.useInternationalization,
-        message: 'Enter the C# project name (used to find resource file):',
-        group: "Internationalization"
-    },
+  {
+    type: 'confirm',
+    name: 'useRouter',
+    message: 'Use router with vue-router integrated with Neutronium?',
+    default: false,
+  },
+  {
+    type: 'confirm',
+    name: 'useInternationalization',
+    message: 'Use internationalization with vue-i18n integrated with Neutronium?',
+    default: false,
+    group: "Internationalization"
+  },
+  {
+    type: 'input',
+    name: 'projectName',
+    when: answer => answer.useInternationalization,
+    message: 'C# namespace containing resources:',
+    group: "Internationalization"
+  },
+  {
+    type: 'input',
+    name: 'nameSpace',
+    default: answer => answer.projectName,
+    when: answer => answer.useInternationalization,
+    message: 'C# namespace containing resources:',
+    group: "Internationalization"
+  },
+  {
+    type: 'input',
+    name: 'exeName',
+    default: answer => answer.projectName,
+    when: answer => answer.useInternationalization,
+    message: 'C# exe name:',
+    group: "Internationalization"
+  },
 ]
