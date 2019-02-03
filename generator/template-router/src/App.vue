dc7efad621de0a1f7b40e10d992bff392067ca99
desmaisons_david <desmaisons_david@hotmail.com>
2019-02-03 11:59:14 -0200

    Adding routing changes in App.vue

---
+++ generator/template-router/src/App.vue
@@ -0,0 +1,14 @@
+---
+extend: '@vue/cli-service/generator/template/src/App.vue'
+replace:
+  - !!js/regexp /export default {/
+---
+
+<%# REPLACE %>
+export default {
+  watch: {
+    "viewModel.Router.Route": function(path) {
+      if (path) this.$router.push({ path });
+    },
+  },
+<%# END_REPLACE %>
--- generator/template-router/src/routeDefinitions.js
+++ generator/template-router/src/routeDefinitions.js
@@ -1,11 +1,10 @@
 // Returns routes here: [{name, component, menu}...]
 // name: display name of the route
 // component: the corresponding component
-// optional: menu.icon: icon for navigation
 
 const routeDefinitions = [
-/*   { name: "main", component: main, menu: { icon: "fa-television" } },
-  { name: "about", component: about, menu: { icon: "info" } } */
+/*   { name: "main", component: main },
+  { name: "about", component: about } */
 ];
 
 export default routeDefinitions;
